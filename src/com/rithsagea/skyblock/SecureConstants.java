dd9aefaea8c5b73b7b6884ae81510ffdc0b69b0c
Rithsagea <rithsagea@gmail.com>
2019-11-17 10:50:57 -0500

    hahaha did you really think i'd give you my passwords

---
+++ src/com/rithsagea/skyblock/SecureConstants.java
@@ -0,0 +1,18 @@
+package com.rithsagea.skyblock;
+
+public class SecureConstants {
+	//link to database
+	public static final String databaseLink = "";
+	
+	//name of table to write to
+	public static final String table = "";
+	
+	//hypixel api key
+	public static final String key =  "";
+	
+	//username
+	public static final String user = "";
+	
+	//password
+	public static final String password = "";
+}
